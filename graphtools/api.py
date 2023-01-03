9e50b173691921b69d9e21a179008bd81fa0c37e
Scott Gigante <scottgigante@users.noreply.github.com>
2023-01-03 00:27:19 -0500

    Update api.py

--- graphtools/api.py
+++ graphtools/api.py
@@ -256,7 +256,7 @@ def Graph(
         else:
             msg = msg + " and PyGSP inheritance"
 
-    _logger.debug(msg)
+    _logger.log_debug(msg)
 
     class_names = [p.__name__.replace("Graph", "") for p in parent_classes]
     try:
@@ -274,7 +274,7 @@ def Graph(
                 pass
 
     # build graph and return
-    _logger.debug(
+    _logger.log_debug(
         "Initializing {} with arguments {}".format(
             parent_classes,
             ", ".join(
